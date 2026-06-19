e9653fb1270d4c31af737804917a51e903b0a1da
pyratin <pyratin@hotmail.com>
2026-06-19 20:27:32 +0530

    Sort borderOutline

--- source/browser/Route_/Round/Control/Sort.js
+++ source/browser/Route_/Round/Control/Sort.js
@@ -12,12 +12,14 @@ import Button from '#browser/Component/Button';
 const Sort = ({ onSortTrigger }) => {
   useExtend({ LayoutContainer, Text, Graphics });
 
-  const { handPlayedFlag, handSortTypeIndexSet } = useStore(
-    useShallow(({ round: { handPlayed }, handSortTypeIndexSet }) => ({
-      handPlayedFlag: !!handPlayed,
-      handSortTypeIndexSet
-    }))
-  );
+  const { borderOutlineTexture, handPlayedFlag, handSortTypeIndexSet } =
+    useStore(
+      useShallow(({ bundle, round: { handPlayed }, handSortTypeIndexSet }) => ({
+        borderOutlineTexture: bundle.miscellaneous.borderOutline,
+        handPlayedFlag: !!handPlayed,
+        handSortTypeIndexSet
+      }))
+    );
 
   const ref = useRef(undefined);
 
@@ -25,45 +27,71 @@ const Sort = ({ onSortTrigger }) => {
     <pixiLayoutContainer
       ref={ref}
       layout={{
-        position: 'relative',
         flex: 1,
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        gap: 2,
-        padding: 2,
         borderWidth: 0,
         borderColor: 0xff0000
       }}
     >
-      <pixiLayoutContainer layout={{}}>
-        <pixiText
-          text='Sort Hand'
-          layout={{}}
-          style={{ fontFamily: 'm6x11plus', fontSize: 24, fill: 0xffffff }}
-          alpha={0.8}
-        />
-      </pixiLayoutContainer>
+      <pixiNineSliceSprite
+        texture={borderOutlineTexture}
+        leftWidth={16}
+        topHeight={16}
+        rightWidth={16}
+        bottomHeight={16}
+        layout={{ position: 'absolute', width: '100%', height: '100%' }}
+        tint={0xffffff}
+        alpha={0.25}
+      />
 
       <pixiLayoutContainer
-        layout={{ width: '100%', justifyContent: 'center', gap: 10 }}
+        layout={{
+          flexDirection: 'column',
+          gap: 5,
+          padding: 20,
+          borderWidth: 0,
+          borderColor: 0x00ff00
+        }}
       >
-        {['rank', 'suit'].map((key, index) => (
-          <Button
-            key={key}
-            text={_.capitalize(key)}
-            fontSize={24}
-            padding={{ padding: 2 }}
-            borderRadius={4}
-            backgroundColor={0xf69000}
-            disableFlag={handPlayedFlag}
-            onPointerTap={() => {
-              handSortTypeIndexSet(index);
-
-              onSortTrigger(true);
-            }}
+        <pixiLayoutContainer
+          layout={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderWidth: 0,
+            borderColor: 0x00ff00
+          }}
+        >
+          <pixiText
+            text='Sort Hand'
+            layout={{}}
+            style={{ fontFamily: 'm6x11plus', fontSize: 24, fill: 0xffffff }}
+            alpha={0.8}
           />
-        ))}
+        </pixiLayoutContainer>
+
+        <pixiLayoutContainer
+          layout={{
+            gap: 10,
+            borderWidth: 0,
+            borderColor: 0xff0000
+          }}
+        >
+          {['rank', 'suit'].map((key, index) => (
+            <Button
+              key={key}
+              text={_.capitalize(key)}
+              fontSize={24}
+              padding={{ padding: 2 }}
+              borderRadius={4}
+              backgroundColor={0xf69000}
+              disableFlag={handPlayedFlag}
+              onPointerTap={() => {
+                handSortTypeIndexSet(index);
+
+                onSortTrigger(true);
+              }}
+            />
+          ))}
+        </pixiLayoutContainer>
       </pixiLayoutContainer>
     </pixiLayoutContainer>
   );
