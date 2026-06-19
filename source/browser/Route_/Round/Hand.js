d27fd97c49477c67870c698dd1674dd38337a41d
pyratin <pyratin@hotmail.com>
2026-06-19 14:38:11 +0530

    finetuning swingX

--- source/browser/Route_/Round/Hand.js
+++ source/browser/Route_/Round/Hand.js
@@ -46,7 +46,7 @@ const cardTransformGet = (index, hand, cardDimension, containerElement) => {
   const swingX = cardDimension.height * Math.sin(angleRad);
 
   return {
-    x: offset + cardWidth * index + cardDimension.width / 2 - swingX * 0.2,
+    x: offset + cardWidth * index + cardDimension.width / 2,
     y:
       Math.abs(_index * 4) +
       (() => {
